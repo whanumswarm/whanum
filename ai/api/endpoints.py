a5b020139697beb316d3fb49cf49562094308396
POINTX <eduardermakov769@gmail.com>
2025-11-03 00:21:06 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        DIMDOX
+import os        POINTX
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
